f6cc5222f0da099d66e5068e74fc6395510af3ca
kev-skywalker <kevinnkouimi@gmail.com>
2024-02-05 23:50:13 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,10 +14,7 @@ variable "projet_device_name" {
   type        = string
   description = "Device name"
 }
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
+
 
 variable "projet_sg" {
   type    = string
